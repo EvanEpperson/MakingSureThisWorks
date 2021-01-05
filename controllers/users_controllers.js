efed3a3882e65e209c49e3663c5657db42ffd85e
Evan Epperson <evanepperson@Evans-MacBook-Pro-2.local>
2021-01-05 16:34:32 -0600

    fixing sign in

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -14,7 +14,7 @@ users.post('/', (req, res) => {
     req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
     User.create(req.body, (err, createdUser) => {
         console.log('the user was created:', createdUser);
-        // res.redirect('/')
+        res.redirect('/users/new')
     })
 })
 
