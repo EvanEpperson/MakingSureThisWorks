33b70f1d1caf31b2afaa48ab10963fa284b99820
Evan Epperson <evanepperson@pop-os.localdomain>
2021-01-06 20:00:09 -0600

    removing admin create new

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -3,22 +3,22 @@ const express = require("express");
 const admin = express.Router();
 const Admin = require("../models/admin.js");
 
-admin.get("/new", (req, res) => {
-  res.render("admin/admin.ejs", {
-    currentAdmin: req.session.currentAdmin,
-  });
-});
+// admin.get("/new", (req, res) => {
+//   res.render("admin/admin.ejs", {
+//     currentAdmin: req.session.currentAdmin,
+//   });
+// });
 
-admin.post("/", (req, res) => {
-  req.body.password = bcrypt.hashSync(
-    req.body.password,
-    bcrypt.genSaltSync(10)
-  );
-  Admin.create(req.body, (err, createdAdmin) => {
-    console.log("the user was created:", createdAdmin);
-    res.redirect("/");
-  });
-});
+// admin.post("/", (req, res) => {
+//   req.body.password = bcrypt.hashSync(
+//     req.body.password,
+//     bcrypt.genSaltSync(10)
+//   );
+//   Admin.create(req.body, (err, createdAdmin) => {
+//     console.log("the user was created:", createdAdmin);
+//     res.redirect("/");
+//   });
+// });
    
 
 
