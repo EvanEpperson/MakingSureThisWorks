60eff001cd280ae70aba17491ee520c3c5f515b4
Evan Epperson <evanepperson@pop-os.localdomain>
2021-01-07 10:53:44 -0600

    saving for seed

--- controllers/main_controller.js
+++ controllers/main_controller.js
@@ -41,68 +41,68 @@ main.get("/:index",  (req, res) => {
 });
 
 
-main.get('/setup/seed', (req, res) => {
-    Main.create(
-      [
-        {
-          firstName: "Matt",
-          lastName: "Huntington",
-          specializations: "Coded the World in 6 days and on the 7th he rested. ",
-          phone: "225",
-          email: "nothing yet",
-          time: "east coast",
-          image:
-            "https://media-exp1.licdn.com/dms/image/C5603AQHpFVJPAnrOmw/profile-displayphoto-shrink_800_800/0/1598280158233?e=1614211200&v=beta&t=9oPg75Pdb6UsIEkjSZGbXAK9-cQi733tbjOIENCui1s",
-          imagea:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkxtbviCaL2IHaeTGsvzF9STDJcqLIWRj1kw&usqp=CAU",
-          imageb:
-            "https://media1.tenor.com/images/49fc1124c648a419e536903d3f9492b2/tenor.gif?itemid=13987172",
-        },
-        {
-          firstName: "Jerrica ",
-          lastName: "Bobadilla",
-          specializations: "Created Bootstrap and then stopped using it. ",
-          phone: "224",
-          email: "nothing yet ",
-          time: "west coast",
-          image:
-            "https://media-exp1.licdn.com/dms/image/C4D03AQEI1pYFVP34hg/profile-displayphoto-shrink_800_800/0/1517038693917?e=1614211200&v=beta&t=YdeNjighsMNv7DDBUvVClAgIbIFSL0Ht6WG2agboDvc",
-          imagea:
-            "https://d2sis3lil8ndrq.cloudfront.net/books/3b63423f-d0e2-4446-8a45-cab2cf783551.png",
-          imageb: "https://media1.giphy.com/media/13FrpeVH09Zrb2/giphy.gif",
-        },
-        {
-          firstName: "Brendan ",
-          lastName: "Quirk",
-          specializations: "Thomas the Train has nothing on Brendan when coding on Rails",
-          phone: "223",
-          email: "nothing yet",
-          time: "east Coast",
-          image:
-            "https://media-exp1.licdn.com/dms/image/C4D03AQGiEssONa97gQ/profile-displayphoto-shrink_800_800/0/1543885252192?e=1614211200&v=beta&t=RyPDYHs0AE0sW89PPM7GnbEIlqWl-0WBo012Q2Kk1do",
-          imagea:
-            "https://cloudinary-res.cloudinary.com/image/upload/c_fill,w_770/dpr_3.0,f_auto,fl_lossy,q_auto/Animations_with_HTML5_2000x1100_v2.gif",
-          imageb: "https://media1.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif",
-        },
-        {
-          firstName: "Leland ",
-          lastName: "Shirley",
-          specializations: "Can code faster than Jordan Walke at React while coding in Vanilla JavaScript.",
-          phone: "222",
-          email: "nothing yet",
-          time: "West Coast",
-          image:
-            "https://media-exp1.licdn.com/dms/image/C4E03AQFyGup6DfMYlQ/profile-displayphoto-shrink_800_800/0/1603986133745?e=1614211200&v=beta&t=_mIaNje3PFZwq-TiKr8Dl9P363YUh6qPcG69rgtB7ak",
-          imagea:
-            "https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1439,w_2560,x_0,y_0/dpr_2.0/c_limit,w_740/fl_lossy,q_auto/v1492203966/articles/2013/12/12/i-took-the-one-hour-of-code-challenge/131210-coding-for-dummies-tease_ohllsl",
-          imageb: "https://media2.giphy.com/media/3ornjIhZGFWpbcGMAU/200.gif",
-        },
-      ],
-      (err, data) => {
-        res.redirect("/main");
-      }
-    );
-})
+// main.get('/setup/seed', (req, res) => {
+//     Main.create(
+//       [
+//         {
+//           firstName: "Matt",
+//           lastName: "Huntington",
+//           specializations: "Coded the World in 6 days and on the 7th he rested. ",
+//           phone: "225",
+//           email: "nothing yet",
+//           time: "east coast",
+//           image:
+//             "https://media-exp1.licdn.com/dms/image/C5603AQHpFVJPAnrOmw/profile-displayphoto-shrink_800_800/0/1598280158233?e=1614211200&v=beta&t=9oPg75Pdb6UsIEkjSZGbXAK9-cQi733tbjOIENCui1s",
+//           imagea:
+//             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkxtbviCaL2IHaeTGsvzF9STDJcqLIWRj1kw&usqp=CAU",
+//           imageb:
+//             "https://media1.tenor.com/images/49fc1124c648a419e536903d3f9492b2/tenor.gif?itemid=13987172",
+//         },
+//         {
+//           firstName: "Jerrica ",
+//           lastName: "Bobadilla",
+//           specializations: "Created Bootstrap and then stopped using it. ",
+//           phone: "224",
+//           email: "nothing yet ",
+//           time: "west coast",
+//           image:
+//             "https://media-exp1.licdn.com/dms/image/C4D03AQEI1pYFVP34hg/profile-displayphoto-shrink_800_800/0/1517038693917?e=1614211200&v=beta&t=YdeNjighsMNv7DDBUvVClAgIbIFSL0Ht6WG2agboDvc",
+//           imagea:
+//             "https://d2sis3lil8ndrq.cloudfront.net/books/3b63423f-d0e2-4446-8a45-cab2cf783551.png",
+//           imageb: "https://media1.giphy.com/media/13FrpeVH09Zrb2/giphy.gif",
+//         },
+//         {
+//           firstName: "Brendan ",
+//           lastName: "Quirk",
+//           specializations: "Thomas the Train has nothing on Brendan when coding on Rails",
+//           phone: "223",
+//           email: "nothing yet",
+//           time: "east Coast",
+//           image:
+//             "https://media-exp1.licdn.com/dms/image/C4D03AQGiEssONa97gQ/profile-displayphoto-shrink_800_800/0/1543885252192?e=1614211200&v=beta&t=RyPDYHs0AE0sW89PPM7GnbEIlqWl-0WBo012Q2Kk1do",
+//           imagea:
+//             "https://cloudinary-res.cloudinary.com/image/upload/c_fill,w_770/dpr_3.0,f_auto,fl_lossy,q_auto/Animations_with_HTML5_2000x1100_v2.gif",
+//           imageb: "https://media1.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif",
+//         },
+//         {
+//           firstName: "Leland ",
+//           lastName: "Shirley",
+//           specializations: "Can code faster than Jordan Walke at React while coding in Vanilla JavaScript.",
+//           phone: "222",
+//           email: "nothing yet",
+//           time: "West Coast",
+//           image:
+//             "https://media-exp1.licdn.com/dms/image/C4E03AQFyGup6DfMYlQ/profile-displayphoto-shrink_800_800/0/1603986133745?e=1614211200&v=beta&t=_mIaNje3PFZwq-TiKr8Dl9P363YUh6qPcG69rgtB7ak",
+//           imagea:
+//             "https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1439,w_2560,x_0,y_0/dpr_2.0/c_limit,w_740/fl_lossy,q_auto/v1492203966/articles/2013/12/12/i-took-the-one-hour-of-code-challenge/131210-coding-for-dummies-tease_ohllsl",
+//           imageb: "https://media2.giphy.com/media/3ornjIhZGFWpbcGMAU/200.gif",
+//         },
+//       ],
+//       (err, data) => {
+//         res.redirect("/main");
+//       }
+//     );
+// })
 
 
 
--- controllers/student_controller.js
+++ controllers/student_controller.js
@@ -88,7 +88,7 @@ student.get('/:index', (req, res) => {
         })
     })
 })
-// last route to post everything 
+// route to create another student , only have this for students because we can't create another Main Teacher 
 student.post('/', (req, res) => {
   Student.create(req.body, (error, createdStudent ) => {
     res.redirect('/student')
@@ -98,91 +98,91 @@ student.post('/', (req, res) => {
 
 
 
-student.get("/setup/seed", (req, res) => {
-  Student.create(
-    [
-      {
-        firstName: "Banu",
-        lastName: "Tryon",
-        specializations:
-          "Made the World Beatufiul with CSS, Then Commented it Out",
-        phone: "225",
-        email: "nothing yet",
-        time: "West Coast",
-        image:
-          "https://media-exp1.licdn.com/dms/image/C5603AQErZ1mG03VOuA/profile-displayphoto-shrink_800_800/0/1607619551717?e=1614816000&v=beta&t=Ds35diTR9tELCR4egU3xZD00QKuOZKMdhrNjpCZnhE4",
-        imageupload: " ",
-        imagea:
-          "https://miro.medium.com/max/2560/1*v0VfsQyWTJ-5eOo5kcjfLg@2x.jpeg",
-        imageb: "https://media0.giphy.com/media/XZYR46vpHwqvYdjVMm/giphy.gif",
-        linkedin: "https://www.linkedin.com/in/tryon-experiences"
-      },
-      {
-        firstName: "AJ",
-        lastName: "Good Luck",
-        specializations:
-          "Created Linux, then let Linus Torvalds take the credit",
-        phone: "224",
-        email: "nothing yet ",
-        time: "East Coast",
-        image:
-          "https://media-exp1.licdn.com/dms/image/C5103AQGfVPKYLl3uTw/profile-displayphoto-shrink_800_800/0/1516353900370?e=1614816000&v=beta&t=ElC4bhCKOp74KVKgqEUPVrmHOHhXXpVOEIZfC-oYk5Q",
-        imagea:
-          "https://upload.wikimedia.org/wikipedia/commons/9/9f/Vimlogo.svg",
-        imageb:
-          "https://steamuserimages-a.akamaihd.net/ugc/518259462797828234/08E759FF7914A201100BA0A31FC4940022D09EE5/",
-        linkedin: "https://www.linkedin.com/in/ajigh/",
-      },
-      {
-        firstName: "Anthony",
-        lastName: "Ku",
-        specializations: "All Around Monster, Literal God Among Meir Mortals",
-        phone: "223",
-        email: "nothing yet",
-        time: "east Coast",
-        image:
-          "https://media-exp1.licdn.com/dms/image/C5603AQHNoePwicZHUQ/profile-displayphoto-shrink_800_800/0/1607029530909?e=1614816000&v=beta&t=yMzTg39Qt_vni0gEPlqT38Jg0fKtw32txmOhu4LYQCg",
-        imagea:
-          "https://i.pinimg.com/originals/24/0b/bb/240bbb8b6e90cb1a0ac5165c2dd6fc27.jpg",
-        imageb:
-          "https://media.tenor.com/images/6e7c95cea52bcc146ac671742c8f1ad5/tenor.gif",
-        linkedin: "https://www.linkedin.com/in/anthonyycku/",
-      },
-      {
-        firstName: "Malik",
-        lastName: "Ajose",
-        specializations: "The CEO, Your not fooling anyone",
-        phone: "222",
-        email: "nothing yet",
-        time: "West Coast",
-        image:
-          "https://media-exp1.licdn.com/dms/image/C5603AQFCBwYW9QawVA/profile-displayphoto-shrink_800_800/0/1606690666214?e=1614816000&v=beta&t=YdOyA8Odc9TvuSYqBy250rur-jOc50WSWqAHTK2BjXM",
-        imagea:
-          "https://www.tvovermind.com/wp-content/uploads/2017/01/UndercoverBoss.jpg",
-        imageb:
-          "https://i.pinimg.com/originals/78/b9/cc/78b9cc43706c49fadf85fee50243ed47.gif",
-        linkedin: "https://www.linkedin.com/in/ajose-malik/",
-      },
-      {
-        firstName: "Hunter",
-        lastName: "Wallen",
-        specializations:
-          "Master of all things JS. Don't ask any questions before he's had his coffee.",
-        phone: "222",
-        email: "nothing yet",
-        time: "East Coast",
-        image:
-          "https://media-exp1.licdn.com/dms/image/C5603AQGbZY3OCY0lKQ/profile-displayphoto-shrink_800_800/0/1607033788620?e=1614816000&v=beta&t=f4JO0qLiEPrg-ikeLgCmTxVwtlTnWpBi9kF5rtIk3OY",
-        imagea: "https://cdn.mos.cms.futurecdn.net/EzgdmaCQuT84bgDL4fhXZS.jpg",
-        imageb: "https://media1.giphy.com/media/TI32JwHmWQEi4/giphy.gif",
-        linkedin: "https://www.linkedin.com/in/hunter-wallen/",
-      },
-    ],
-    (err, data) => {
-      res.redirect("/student");
-    }
-  );
-});
+// student.get("/setup/seed", (req, res) => {
+//   Student.create(
+//     [
+//       {
+//         firstName: "Banu",
+//         lastName: "Tryon",
+//         specializations:
+//           "Made the World Beatufiul with CSS, Then Commented it Out",
+//         phone: "225",
+//         email: "nothing yet",
+//         time: "West Coast",
+//         image:
+//           "https://media-exp1.licdn.com/dms/image/C5603AQErZ1mG03VOuA/profile-displayphoto-shrink_800_800/0/1607619551717?e=1614816000&v=beta&t=Ds35diTR9tELCR4egU3xZD00QKuOZKMdhrNjpCZnhE4",
+//         imageupload: " ",
+//         imagea:
+//           "https://miro.medium.com/max/2560/1*v0VfsQyWTJ-5eOo5kcjfLg@2x.jpeg",
+//         imageb: "https://media0.giphy.com/media/XZYR46vpHwqvYdjVMm/giphy.gif",
+//         linkedin: "https://www.linkedin.com/in/tryon-experiences"
+//       },
+//       {
+//         firstName: "AJ",
+//         lastName: "Good Luck",
+//         specializations:
+//           "Created Linux, then let Linus Torvalds take the credit",
+//         phone: "224",
+//         email: "nothing yet ",
+//         time: "East Coast",
+//         image:
+//           "https://media-exp1.licdn.com/dms/image/C5103AQGfVPKYLl3uTw/profile-displayphoto-shrink_800_800/0/1516353900370?e=1614816000&v=beta&t=ElC4bhCKOp74KVKgqEUPVrmHOHhXXpVOEIZfC-oYk5Q",
+//         imagea:
+//           "https://upload.wikimedia.org/wikipedia/commons/9/9f/Vimlogo.svg",
+//         imageb:
+//           "https://steamuserimages-a.akamaihd.net/ugc/518259462797828234/08E759FF7914A201100BA0A31FC4940022D09EE5/",
+//         linkedin: "https://www.linkedin.com/in/ajigh/",
+//       },
+//       {
+//         firstName: "Anthony",
+//         lastName: "Ku",
+//         specializations: "All Around Monster, Literal God Among Meir Mortals",
+//         phone: "223",
+//         email: "nothing yet",
+//         time: "east Coast",
+//         image:
+//           "https://media-exp1.licdn.com/dms/image/C5603AQHNoePwicZHUQ/profile-displayphoto-shrink_800_800/0/1607029530909?e=1614816000&v=beta&t=yMzTg39Qt_vni0gEPlqT38Jg0fKtw32txmOhu4LYQCg",
+//         imagea:
+//           "https://i.pinimg.com/originals/24/0b/bb/240bbb8b6e90cb1a0ac5165c2dd6fc27.jpg",
+//         imageb:
+//           "https://media.tenor.com/images/6e7c95cea52bcc146ac671742c8f1ad5/tenor.gif",
+//         linkedin: "https://www.linkedin.com/in/anthonyycku/",
+//       },
+//       {
+//         firstName: "Malik",
+//         lastName: "Ajose",
+//         specializations: "The CEO, Your not fooling anyone",
+//         phone: "222",
+//         email: "nothing yet",
+//         time: "West Coast",
+//         image:
+//           "https://media-exp1.licdn.com/dms/image/C5603AQFCBwYW9QawVA/profile-displayphoto-shrink_800_800/0/1606690666214?e=1614816000&v=beta&t=YdOyA8Odc9TvuSYqBy250rur-jOc50WSWqAHTK2BjXM",
+//         imagea:
+//           "https://www.tvovermind.com/wp-content/uploads/2017/01/UndercoverBoss.jpg",
+//         imageb:
+//           "https://i.pinimg.com/originals/78/b9/cc/78b9cc43706c49fadf85fee50243ed47.gif",
+//         linkedin: "https://www.linkedin.com/in/ajose-malik/",
+//       },
+//       {
+//         firstName: "Hunter",
+//         lastName: "Wallen",
+//         specializations:
+//           "Master of all things JS. Don't ask any questions before he's had his coffee.",
+//         phone: "222",
+//         email: "nothing yet",
+//         time: "East Coast",
+//         image:
+//           "https://media-exp1.licdn.com/dms/image/C5603AQGbZY3OCY0lKQ/profile-displayphoto-shrink_800_800/0/1607033788620?e=1614816000&v=beta&t=f4JO0qLiEPrg-ikeLgCmTxVwtlTnWpBi9kF5rtIk3OY",
+//         imagea: "https://cdn.mos.cms.futurecdn.net/EzgdmaCQuT84bgDL4fhXZS.jpg",
+//         imageb: "https://media1.giphy.com/media/TI32JwHmWQEi4/giphy.gif",
+//         linkedin: "https://www.linkedin.com/in/hunter-wallen/",
+//       },
+//     ],
+//     (err, data) => {
+//       res.redirect("/student");
+//     }
+//   );
+// });
 
 
 
